d146f17983c1364bdb9b91fe17897d508145f92d
Michael Wender <michael@michaelwender.com>
2017-07-19 19:17:27 +0000

    Adding check for config.sh and setting var for present working dir

--- web-form-to-ims/web-form-to-ims.sh
+++ web-form-to-ims/web-form-to-ims.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-source ~/scripts/config.sh
+# Get current directory (not bulletproof, source: http://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/)
+PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Check for `config.sh`
+if ! $(source $PWD/../config.sh 2>/dev/null); then
+	echo 'ERROR: No configuration found! Please setup `config.sh` with the proper vars defined.'
+	exit
+fi
+
+source $PWD/../config.sh
 
 SITELIST=($(ls -lh $SITESTORE | awk '{print $9}'))
 
-
 for SITE in ${SITELIST[@]}; do
+	# Check for XML form entries
 	if ! $(cd $SITESTORE/$SITE/$XMLFILES 2>/dev/null); then
 		printf "\e[33m\e[1mNOTE:\e[0m $SITE - no XML form entries found. Continuing...\n"
 		continue
@@ -15,7 +24,7 @@ for SITE in ${SITELIST[@]}; do
 	printf "\e[32m\e[1mSUCCESS:\e[0m $SITE has $TOTALENTRIES XML form entries.\n"
 	cd $SITESTORE/$SITE/$XMLFILES
 	FILELIST=($(ls --hide=.htaccess -lh . | awk '{print $9}'))
-	#for FILE in ${FILELIST[@]}; do
-	#	printf "Found $FILE\n"
-	#done
+	for FILE in ${FILELIST[@]}; do
+		printf "Found $FILE\n"
+	done
 done
